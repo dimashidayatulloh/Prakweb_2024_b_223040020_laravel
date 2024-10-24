72267a8f68008ba53d067f9203421e568c23570c
dimashidayatulloh <dimaskalem07@gmail.com>
2024-10-25 00:04:15 +0700

    Laravel #5 Blade Component

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,24 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Halaman Contact</title>
-</head>
-
-<body>
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <a href="/blog">Blog</a>
-    <a href="/contact">Contact</a>
-    <h1>Halaman Contact</h1>
-    <ul>
-        <li><a href="https://instagram.com/dimas.frando">Instagram</a></li>
-        <li><a href="https://x.com/dimas.frando"></a>X</li>
-        <li><a href="https://youtube.com/@dimas.frando">YouTube</a></li>
-    </ul>
-</body>
-
-</html>
+<x-layout>
+    <!-- Your content -->
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <p>Halaman Contact</p>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['title' => 'Home Page']);
 });
 
 Route::get('/about', function () {
-    return view('about', ['nama' => 'Dimas Hidayatulloh']);
+    return view('about', ['name' => 'Dimas Hidayatulloh', 'title' => 'About']);
 });
 
 // Buat 2 rute baru
 Route::get('/blog', function () {
-    return view('blog');
+    return view('blog', ['title' => 'Blog']);
 });
 
 Route::get('/contact', function () {
-    return view('contact');
+    return view('contact', ['title' => 'Contact']);
 });
