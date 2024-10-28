c3f9b37b651be761852402d769e1787b7ebd7171
dimashidayatulloh <dimaskalem07@gmail.com>
2024-10-28 13:15:05 +0700

    Laravel #6 View Data

--- resources/views/blog.blade.php
+++
@@ -1,5 +0,0 @@
-<x-layout>
-    <!-- Your content -->
-    <x-slot:title>{{ $title }}</x-slot:title>
-    <p>Welcome to my blog!</p>
-</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,8 +12,48 @@ Route::get('/about', function () {
 });
 
 // Buat 2 rute baru
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' => [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Dimas Hidayatulloh',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate aliquid suscipit, minus iusto, tempore quidem laborum accusamus provident nam quisquam voluptas adipisci reiciendis quis, sit eius delectus. Voluptas, unde nihil.'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Dimas Hidayatulloh',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate aliquid suscipit, minus iusto, tempore quidem laborum accusamus provident nam quisquam voluptas adipisci reiciendis quis, sit eius delectus. Voluptas, unde nihil.'
+        ]
+    ]]);
+});
+
+Route::get('/posts/{slug}', function ($slug) {
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Dimas Hidayatulloh',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate aliquid suscipit, minus iusto, tempore quidem laborum accusamus provident nam quisquam voluptas adipisci reiciendis quis, sit eius delectus. Voluptas, unde nihil.'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Dimas Hidayatulloh',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate aliquid suscipit, minus iusto, tempore quidem laborum accusamus provident nam quisquam voluptas adipisci reiciendis quis, sit eius delectus. Voluptas, unde nihil.'
+        ]
+    ];
+
+    $post = Arr::first($posts, function ($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
