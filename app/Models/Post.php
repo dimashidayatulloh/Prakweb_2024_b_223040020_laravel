2c2860e12fdb3518addfd5a805d52b4697bd2ccd
dimashidayatulloh <dimaskalem07@gmail.com>
2024-11-03 21:34:28 +0700

    Laravel #9 ORM & Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,37 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Post
+class Post extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'Dimas Hidayatulloh',
-                'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate aliquid suscipit, minus iusto, tempore quidem laborum accusamus provident nam quisquam voluptas adipisci reiciendis quis, sit eius delectus. Voluptas, unde nihil.'
-            ],
-            [
-                'id' => 2,
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel 2',
-                'author' => 'Dimas Hidayatulloh',
-                'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate aliquid suscipit, minus iusto, tempore quidem laborum accusamus provident nam quisquam voluptas adipisci reiciendis quis, sit eius delectus. Voluptas, unde nihil.'
-            ]
-        ];
-    }
-
-    public static function find($slug): array
-    {
-        $post = Arr::first(static::all(), fn($post) => $post['slug'] = $slug);
-        if (!$post) {
-            abort(404);
-        }
-
-        return $post;
-    }
+    protected $fillable = ['title', 'author', 'slug', 'body'];
 }
